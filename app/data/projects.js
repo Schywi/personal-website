d9ad73849040256ab10c598631e673cdfd7d395c
Schywi <lucasmirandoliveira@gmail.com>
2020-10-10 21:42:14 -0300

    second commit

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,7 +4,7 @@ const data = [
     title: 'Aparecida Nutritionist',
     subtitle: 'A website to calculate the BMI of patients',
     link: 'https://lucasm-aparecida-nutritionist.netlify.app/',
-    image: '/images/projects/aparecida.jpg',
+    image: './images/projects/aparecida.jpg',
     date: '2020-02-28',
     desc:
         'A website to calculate the BMI of patients, '
@@ -17,7 +17,7 @@ const data = [
     title: 'GitHub Explorer',
     subtitle: 'GoStack Project',
     link: 'https://lucasm-github-explorer.netlify.app/',
-    image: '/images/projects/githubexplorer.jpg',
+    image: './images/projects/githubexplorer.jpg',
     date: '2020-05-20',
     desc: 'Built in Rocketseat GoStack Bootcamp ,  '
       + 'we can find informations of any github profile and repository by tapying the username  '
@@ -27,7 +27,7 @@ const data = [
     title: 'Twitter Homepage',
     subtitle: 'Adapted from HTML,CSS project to React  ',
     link: 'https://lucasm-homepage-twitter.netlify.app/',
-    image: '/images/projects/twitterhomepage.jpg',
+    image: './images/projects/twitterhomepage.jpg',
     date: '2020-06-12',
     desc: 'React project developed during the flexbox course at Rocketseat '
       + ' with Typescript and styled compontents.',
@@ -36,7 +36,7 @@ const data = [
     title: 'Apeperia Website',
     subtitle: 'Developed during the web accessibility course at alura  ',
     link: 'https://lucasm-apeperia.netlify.app/#',
-    image: '/images/projects/apeperia.jpg',
+    image: './images/projects/apeperia.jpg',
     date: '2020-08-03',
     desc: 'Developed during the web accessibility course at alura with HTML, CSS and Javascript,  '
       + ' Apeperia Website is a fictional startup with an innovative way of buying  '
